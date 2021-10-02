894305769c9ebdaadfa8c3d7888ee8d1e8c1f508
JusticeBrains <justiceduodu14@gmail.com>
2021-10-02 22:26:31 +0000

    Update print statement output
    Modified the display of the output file

--- merge_two_dicts.py
+++ merge_two_dicts.py
@@ -1,17 +1,17 @@
 d1 = {'James': 25, 'Roland': 27, 'Daniel': 19}
-d2 = {'Paul': 45, 'Flex': 39, 'William': 39, 'James': 46}
+d2 = {'Paul': 45, 'Felix': 39, 'William': 39, 'James': 46}
 
 # using dict unpacking approach
 d3 = {**d1, **d2}
-print(d3)
+print(f"Dict unpacking: {d3}")
 
 # using the union operator
 d4 = d1 | d2 
-print(d4)
+print(f"Union operator: {d4}")
 
 # updates d1 with d2 using the update operator (|=)
 # works like the concatenation +=
 d1 |= d2
 
-print(d1)
+print(f"Update operator: {d1}")
 
